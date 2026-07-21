5d1d9e427511aa06f5a7b8bc4bea14bc6419b7af
Gishor04 <rajangishor04@gmail.com>
2026-07-21 21:22:02 +0530

    fix db json api and images

---
+++ api/auth/db.js
@@ -0,0 +1,9 @@
+import fs from "fs";
+
+export default function handler(req, res) {
+    const data = JSON.parse(
+        fs.readFileSync("./db.json", "utf8")
+    );
+
+    res.status(200).json(data);
+}
