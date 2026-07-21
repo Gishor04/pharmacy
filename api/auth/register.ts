4d53a3109887bc37c04669ee36503fa2122cdd0f
Gishor04 <rajangishor04@gmail.com>
2026-07-21 20:38:42 +0530

    add vercel api routes

---
+++ api/auth/register.ts
@@ -0,0 +1,18 @@
+export default async function handler(req: any, res: any) {
+
+    if (req.method !== "POST") {
+        return res.status(405).json({
+            message: "Method not allowed"
+        });
+    }
+
+    const { name, email, password } = req.body;
+
+    return res.status(200).json({
+        message: "Register API working",
+        user: {
+            name,
+            email
+        }
+    });
+}
